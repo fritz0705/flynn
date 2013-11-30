48e04ae85c563ab6af03773535ebeed748d33572
Fritz Grimpen <fritz@grimpen.net>
2013-11-30 19:55:13 +0000

    Implement dumph to generate input for cbor.me

--- flynn/__init__.py
+++ flynn/__init__.py
@@ -9,9 +9,15 @@ def dump(obj, fp):
 def dumps(obj):
 	return flynn.encoder.encode_str(obj)
 
+def dumph(obj):
+	return "".join(hex(n)[2:].rjust(2, "0") for n in dumps(obj))
+
 def load(s):
 	return flynn.decoder.decode(s)
 
 def loads(s):
+	return flynn.decoder.decode(s)
+
+def loadh(s):
 	return flynn.decoder.decode(s)
 
